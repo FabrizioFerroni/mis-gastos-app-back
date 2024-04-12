f8ea1e2a0453b36674a197f00122b3fd1bc541c8
Fabrizio Ferroni <fabrizioferroni@outlook.com>
2024-04-12 13:53:46 +0200

    fix: test lint

--- src/api/service/status.service.ts
+++ src/api/service/status.service.ts
@@ -4,6 +4,8 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class StatusService {
   getAvailability(): Health {
+    console.log('Test linter');
+
     return { status: HealthStatus.AVAILABLE };
   }
 }
