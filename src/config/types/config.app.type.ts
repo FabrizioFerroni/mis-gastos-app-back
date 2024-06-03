456e772f39989f450e6dd055e0c23c99856d3dfd
Fabrizio Ferroni <fabrizioferroni@outlook.com>
2024-06-03 22:48:22 +0200

    refactor: the tests are reset, and the configuration is restored to its pre-infisical state

--- src/config/types/config.app.type.ts
+++ src/config/types/config.app.type.ts
@@ -1,13 +1,33 @@
 export type ConfigApp = {
-  infisical: InfisicalConfig;
   env: string;
+  apiPort: number;
+  redis: ConfigAppRedis;
+  ttl: number;
+  limit: number;
+  max_pass_failures: number;
+  secret_jwt: string;
+  secret_jwt_register: string;
+  mailServiceUrl: string;
+  passPrivateKey: string;
+  database: DbConfig;
+  frontHost: string;
+  hostMethod: string;
+  hostAllowedHeader: string;
+  hostCredentials: boolean;
 };
 
-export type InfisicalConfig = {
-  siteUrl: string;
-  clientId: string;
-  clientSecret: string;
-  projectId: string;
-  environment: string;
-  path: string;
+export type ConfigAppRedis = {
+  host: string;
+  port: number;
+  username: string;
+  password: string;
+  ttl: number;
+};
+
+export type DbConfig = {
+  host: string;
+  port: number;
+  username: string;
+  password: string;
+  database: string;
 };
