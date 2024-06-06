1df92af75dd4ace08ca6bcd65afb855966fdea8a
Fabrizio Ferroni <fabrizioferroni@outlook.com>
2024-06-06 23:14:21 +0200

    chore: create a dtos and repository for categorias

--- src/api/categorias/categorias.module.ts
+++ src/api/categorias/categorias.module.ts
@@ -2,9 +2,18 @@ import { Module } from '@nestjs/common';
 import { CategoriaEntity } from './entity/categoria.entyty';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { TransformDto } from '@/shared/utils';
+import { CategoriaRepository } from './repository/categoria.repository';
+import { CategoriaInterfaceRepository } from './repository/categoria.interface.repository';
 
 @Module({
   imports: [TypeOrmModule.forFeature([CategoriaEntity])],
-  providers: [TransformDto],
+  providers: [
+    {
+      provide: CategoriaInterfaceRepository,
+      useClass: CategoriaRepository,
+    },
+
+    TransformDto,
+  ],
 })
 export class CategoriasModule {}
---
+++ src/api/categorias/repository/categoria.interface.repository.ts
@@ -0,0 +1,26 @@
+import { Injectable } from '@nestjs/common';
+import { BaseAbstractRepository } from '@/config/database/mysql/mysql.base.repository';
+import { UpdateResult } from 'typeorm';
+import { UsuarioEntity } from '@/api/usuario/entity/usuario.entity';
+import { CategoriaEntity } from '../entity/categoria.entyty';
+
+@Injectable()
+export abstract class CategoriaInterfaceRepository extends BaseAbstractRepository<CategoriaEntity> {
+  abstract guardar(data: CategoriaEntity): Promise<CategoriaEntity>;
+  abstract obtenerTodos(
+    usuario: UsuarioEntity,
+    skip?: number,
+    take?: number,
+  ): Promise<[CategoriaEntity[], number]>;
+  abstract obtenerPorId(id: string): Promise<CategoriaEntity>;
+  abstract borrar(id: string): Promise<UpdateResult>;
+  abstract actualizar(id: string, data: CategoriaEntity): Promise<UpdateResult>;
+  abstract obtenerRelaciones(): Promise<CategoriaEntity[]>;
+  abstract categoriaYaExiste(
+    nombre: string,
+    id?: string,
+    usuario?: UsuarioEntity,
+  ): Promise<boolean>;
+  abstract existeRegistro(id: string, deleted?: boolean): Promise<boolean>;
+  abstract restaurar(id: string): Promise<UpdateResult>;
+}
