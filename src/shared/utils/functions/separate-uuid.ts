4298946d7aa9293afecfca09f0295443b4986283
Fabrizio Ferroni <fabrizioferroni@outlook.com>
2024-07-24 17:26:39 +0200

    refactor: change key to save on redis for user movement for categories and account

--- src/shared/utils/functions/separate-uuid.ts
+++ src/shared/utils/functions/separate-uuid.ts
@@ -2,5 +2,5 @@ export function separateUUIDUser(user_id: string): string {
   const userSeparateId: string[] = user_id.split('-');
   const lastSegment: string = userSeparateId[4];
   const lastFourChars: string = lastSegment.slice(-4);
-  return `u_${lastFourChars}`;
+  return `user_${lastFourChars}`;
 }
